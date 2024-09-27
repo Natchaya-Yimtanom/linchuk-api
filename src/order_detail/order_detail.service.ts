b1a6df50477be74b0c0ed6a56b8a7a34abfbfb83
Natchaya Yimtanom <natyapim@gmail.com>
2024-09-27 21:04:29 +0700

    fix product not remove

--- src/order_detail/order_detail.service.ts
+++ src/order_detail/order_detail.service.ts
@@ -5,20 +5,27 @@ import { Repository } from 'typeorm';
 import { OrderService } from 'src/order/order.service';
 import { T_ORDER } from 'src/order/entity/order.entity';
 import { OrderDetailDto } from './dto/order_detail.dto';
+import { T_PRODUCT } from 'src/product/entity/product.entity';
 
 @Injectable()
 export class OrderDetailService {
     constructor(
         @InjectRepository(T_ORDER_DETAIL)
-        private readonly orderDetailRepository: Repository<T_ORDER_DETAIL>
+        private readonly orderDetailRepository: Repository<T_ORDER_DETAIL>,
+
+        @InjectRepository(T_PRODUCT)
+        private readonly productRepository: Repository<T_PRODUCT>,
     ) {}
 
     async createMultipleRows(orderId: number, orderDetailDto: OrderDetailDto[]): Promise<any[]> {
+        console.log(orderDetailDto);
         const details = orderDetailDto.map(elm => {
             const detail = this.orderDetailRepository.create(elm);
             detail.order_id = orderId;
             return detail;
         });
+
+        await this.removeFromProduct(orderDetailDto);
 
         return await this.orderDetailRepository.save(details);
       }
@@ -33,5 +40,14 @@ export class OrderDetailService {
     
     async findAll(): Promise<T_ORDER_DETAIL[]> {
         return await this.orderDetailRepository.find();
+    }
+
+    async removeFromProduct(orderDetailDto: OrderDetailDto[]){
+        orderDetailDto.map(async elm => {
+            console.log(elm);
+            const products: T_PRODUCT[] = await this.productRepository.find({ where: {product_id: elm.product_id} });
+            products[0].product_quantity -= elm.quantity;
+            this.productRepository.save(products);
+        });
     }
 }
